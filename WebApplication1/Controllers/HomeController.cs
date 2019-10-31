f6f3b45b349c172e5c06d4474ab4f757976a0938
Dilbert Jazz <dilbertjazz@outlook.com>
2019-10-31 13:06:37 -0500

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRespository _employeeRespository;
+
+        public HomeController(IEmployeeRespository employeeRespository)
+        {
+            _employeeRespository = employeeRespository;
+        }
+
+        public string Index()
+        {
+            return _employeeRespository.GetEmployee(1).Name;
+        }
+
+        public ViewResult Details()
+        {
+            Employee model = _employeeRespository.GetEmployee(1);
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication1/Models/MockEmployeeRespository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRespository : IEmployeeRespository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRespository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee { Id = 1, Name = "Mary", Email = "mary@pragim.com", Department = "HR" },
+                new Employee { Id = 2, Name = "John", Email = "john@pragim.com", Department = "IT" },
+                new Employee { Id = 3, Name = "Sam", Email = "sam@pragim.com", Department = "PR" }
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
